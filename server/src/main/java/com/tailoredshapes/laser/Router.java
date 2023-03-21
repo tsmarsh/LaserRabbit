395efd606aaab48452f1c22217d121f90c05255b
Thomas Marsh <tmarsh@tailoredshapes.com>
2023-03-21 12:04:00 -0400

    Simple api tests passing

--- server/src/main/java/com/tailoredshapes/laser/Router.java
+++ server/src/main/java/com/tailoredshapes/laser/Router.java
@@ -1,17 +1,42 @@
 package com.tailoredshapes.laser;
 
+import com.google.gson.stream.JsonReader;
+import com.tailoredshapes.stash.Stash;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParserBuilder;
+import io.jsonwebtoken.Jwts;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Filter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 import java.util.List;
+import java.util.Set;
+import java.util.jar.JarEntry;
 
+import static com.tailoredshapes.stash.Stash.stash;
+import static com.tailoredshapes.underbar.IO.slurp;
 import static com.tailoredshapes.underbar.UnderBar.list;
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 public interface Router {
     Logger log = LoggerFactory.getLogger(Router.class);
 
     static void route(){
+        before((req, res) -> {
+            String jwt = req.headers("Authorization");
+            if(jwt != null) {
+                String[] parts = jwt.substring(7).split("\\.");
+                Base64.Decoder decoder = Base64.getUrlDecoder();
+                var body =   Stash.parseJSON(slurp(new ByteArrayInputStream(decoder.decode(parts[1]))));
+                req.attribute("user", body.get("sub"));
+            }else{
+                halt(401, "Unauthorized");
+            }
+        });
+
         get("/forms", "application/json", (req,res) -> {
             log.debug("allForms");
             res.type("application/json");
--- server/src/test/java/com/tailoredshapes/laser/MainTest.java
+++ server/src/test/java/com/tailoredshapes/laser/MainTest.java
@@ -1,9 +1,20 @@
 package com.tailoredshapes.laser;
 
+import com.tailoredshapes.laserrabbit.api.ApiClient;
+import com.tailoredshapes.laserrabbit.api.ApiException;
+import com.tailoredshapes.laserrabbit.api.Configuration;
+import com.tailoredshapes.laserrabbit.api.auth.Authentication;
+import com.tailoredshapes.laserrabbit.api.auth.OAuth;
 import com.tailoredshapes.laserrabbit.api.client.DefaultApi;
 import com.tailoredshapes.laserrabbit.api.model.Form;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.function.Executable;
+
+import java.security.Key;
 
 import java.util.List;
 
@@ -14,17 +25,31 @@ import static com.tailoredshapes.underbar.Die.*;
 
 class MainTest {
 
-    static DefaultApi api;
+    static String jws;
 
     @BeforeAll
     static void beforeAll() {
-        api = new DefaultApi();
         Server.start();
+
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        jws = Jwts.builder().setSubject("Joe").signWith(key).compact();
+    }
+
+    @Test
+    void shouldRejectAListRequestWithoutAuth(){
+        DefaultApi api = new DefaultApi();
+
+        assertThrows(ApiException.class, () -> api.allForms());
     }
 
     @Test
     void shouldReturnAnEmptyList() {
         DefaultApi api = new DefaultApi();
+        ApiClient client = api.getApiClient();
+        OAuth formAuth = (OAuth) client.getAuthentication("bearerAuth");
+        formAuth.setAccessToken(jws);
+
         assertEquals(0, rethrow(api::allForms).size());
     }
 
